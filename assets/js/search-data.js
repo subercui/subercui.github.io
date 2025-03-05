e7870c3c588d60d2acdb7c07a30b6c6d5735fb5d
subercui <subercui@gmail.com>
2025-03-05 08:03:28 +0000

    Deploying to gh-pages from @ subercui/subercui.github.io@ba83b94bc633e18cff68b3b043a03c363db7c272 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,18 +44,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2.html";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "projects-project-1",
+        },{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
